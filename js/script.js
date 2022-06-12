64871d853f5bb2f931450e2e8159c548f7d0e6d8
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-06-13 01:36:06 +0300

    feat: Add functionality to navbar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const body = document.querySelector(".body");
 const checkbox = document.getElementById("checkbox");
 const toggleBtn = document.getElementById("nav__toggle--btn");
+const toggleNav = document.querySelector(".nav");
+
+toggleBtn.addEventListener("click", () => {
+  toggleNav.className === "nav"
+    ? (toggleNav.className += " responsive")
+    : (toggleNav.className = "nav");
+});
 
 toggleBtn.addEventListener("click", () => {
   toggleBtn.classList.toggle("open");
